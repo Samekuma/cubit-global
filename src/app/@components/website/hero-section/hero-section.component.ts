6a7ec30a7dd2632af875acf9cec05c4d71476a83
Samuel Ekuma <samuelekumamba@gmail.com>
2024-12-04 02:19:54 +0100

    Added Objectives Components

--- src/app/@components/website/hero-section/hero-section.component.ts
+++ src/app/@components/website/hero-section/hero-section.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSectionComponent implements OnInit {
   fullText: string =
-    'Finding a property isn’t just about square footage—it’s about finding a space where life unfolds, memories are made, and dreams are built!';
+    "Finding the perfect property is about more than space—it's where memories are made and dreams bloom.";
+  // "Finding the perfect property isn’t just about square footage—it's about discovering a place where memories are created, dreams take shape, and life truly unfolds!";
+  // 'Finding a property isn’t just about square footage—it’s about finding a space where life unfolds, memories are made, and dreams are built!';
+
   displayedText: string = '';
-  typingSpeed: number = 120; // Time in milliseconds between typing each character
+  typingSpeed: number = 100; // Time in milliseconds between typing each character
   isTypingComplete: boolean = false;
 
   constructor() {}
@@ -26,7 +29,7 @@ export class HeroSectionComponent implements OnInit {
         this.displayedText += this.fullText[index];
         index++;
       } else {
-        clearInterval(intervalId); // Stop the typing effect when finished
+        clearInterval(intervalId);
         this.isTypingComplete = true;
       }
     }, this.typingSpeed);
