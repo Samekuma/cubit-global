7d9f70ec6e296f15c1a8d9bc6b44665f8e53e63f
Samuel Ekuma <samuelekumamba@gmail.com>
2024-12-24 12:40:50 +0100

    Developed About, Properties, and Services

---
+++ src/app/@components/website/home/featured-properties/featured-properties.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeaturedPropertiesComponent } from './featured-properties.component';
+
+@NgModule({
+  declarations: [FeaturedPropertiesComponent],
+  imports: [CommonModule],
+  exports: [FeaturedPropertiesComponent],
+})
+export class FeaturedPropertiesModule {}
